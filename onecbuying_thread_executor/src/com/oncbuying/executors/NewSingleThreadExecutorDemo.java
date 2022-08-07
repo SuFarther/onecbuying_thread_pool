f687f080226f1db589c45f0107b33646698a1de4
xiaosu <2257346739@qq.com>
2022-08-07 21:18:58 +0800

    newSingleThreadExecutor创建一个单线程化的线程池，它只会用唯一的工作线程来执行任务，保证所有任务按照指定顺序(FIFO, LIFO, 优先级)执行

---
+++ onecbuying_thread_executor/src/com/oncbuying/executors/NewSingleThreadExecutorDemo.java
@@ -0,0 +1,33 @@
+package com.oncbuying.executors;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author 苏东坡
+ * @version 1.0
+ * @ClassName NewSingleThreadExecutorDemo
+ * @company 公司
+ * @Description  结果依次输出，相当于顺序执行各个任务
+ * 创建一个单线程化的线程池，它只会用唯一的工作线程来执行任务，保证所有任务按照指定顺序(FIFO, LIFO, 优先级)执行
+ */
+public class NewSingleThreadExecutorDemo {
+    public static void main(String[] args) {
+        ExecutorService newSingleThreadExecutor = Executors.newSingleThreadExecutor();
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            newSingleThreadExecutor.execute(new Runnable() {
+
+                @Override
+                public void run() {
+                    System.out.println("index:" + index);
+                    try {
+                        Thread.sleep(200);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+}
